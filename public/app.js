74b2264b1ff463877420565050d9129aa3b3a375
lyhottag13 <leonardo@promoevt.com>
2025-07-09 09:59:44 -0700

    Created Buttons

--- public/app.js
+++ public/app.js
@@ -1,21 +1,43 @@
 const holder = document.getElementById('holder');
+const header = document.querySelector('h1');
+
+const imageNames = ['Captura', 'Pareto Visualizer', 'Sistema420'];
+const links = ['https://youtube.com', 'https://amazon.com', 'https://roblox.com'];
+
+const images = [];
+
 
 async function main() {
-    const data = await (await fetch('https://api.adviceslip.com/advice')).json();
-    placeOnDiv()
-    const activity = (await fetchBored()).activity;
-    holder.innerHTML += `<br>${activity}`;
+    imageNames.map((imageName, index) => {
+        images[index] = {};
+        const newImage = new Image();
+        newImage.src = `./assets/${imageName}.webp`;
+        images[index].image = newImage;
+        images[index].link = links[index];
+    });
+    images.forEach(image => {
+        createPanel(image, 'https://google.com');
+    })
+    startClock();
 }
-async function fetchBored() {
-    const res = await fetch('/api/connection');
-    const data = await res.json();
-    return data;
+
+function createPanel(image) {
+    const newPanel = document.createElement('a');
+    newPanel.className = 'app-panel';
+    newPanel.href = image.link;
+
+    const newImage = document.createElement('img');
+    newImage.src = image.image.src;
+    newImage.style.height = '200px';
+
+    newPanel.appendChild(newImage);
+    holder.appendChild(newPanel);
 }
-function placeOnDiv(text) {
-    const newDiv = document.createElement('div');
-    newDiv.className = 'api-panel';
-    newDiv.innerText = text;
-    holder.appendChild(newDiv);
+function startClock() {
+    header.innerText += ` - ${(new Date().toLocaleString())}`;
+    setInterval(() => {
+        header.innerText = `Coto Technology - ${new Date().toLocaleString()}`
+    }, 1000);
 }
 
 main();
